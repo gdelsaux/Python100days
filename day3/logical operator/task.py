8fc2b26ae133cc84b2978b02a4e6d6ee01dfb449
Gaetan <gaetan@pooky.com>
2024-12-30 13:58:31 +0000

    lesson: logical operator

---
+++ day3/logical operator/task.py
@@ -0,0 +1,11 @@
+my_var = 12
+
+# and condition
+print(my_var == 12 and my_var > 6) # expect True
+
+# or
+print(my_var == 11 or my_var == 12) # expect True
+
+#not 
+print(not my_var == 12) # expect False
+print(my_var != 12) # expect False
